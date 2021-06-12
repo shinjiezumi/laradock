15a4a9173321ef8a007c1f514c160f3b70779abe
shinjiezumi <shinjiezumi.work@gmail.com>
2021-06-12 13:40:57 +0900

    Enable asset versioning

--- resources/views/pf/layouts/app.blade.php
+++ resources/views/pf/layouts/app.blade.php
@@ -7,14 +7,14 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=M+PLUS+Rounded+1c">
     <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="shortcut icon" type="image/x-icon" href=""/>
   </head>
   <body data-spy="scroll" data-target="#navbar" data-offset="150">
     <div id="app">
       @yield('content')
     </div>
-    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
 
