d485c346e97b307d5f7f9436d05b21d4227ac193
shinjiezumi <shinjiezumi.work@gmail.com>
2021-06-19 12:59:17 +0900

    Add sample breadcrumbs

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,10 @@
+<?php
+//
+//Breadcrumbs::register('top', function($breadcrumbs) {
+//    $breadcrumbs->push('Top', route('top'));
+//});
+//
+//Breadcrumbs::register('todo', function ($breadcrumbs) {
+//    $breadcrumbs->parent('top');
+//    $breadcrumbs->push('Todoリスト', route('todo'));
+//});
