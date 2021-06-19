8268ed835b7136ee143759eb7dbd2cd25ea0c96c
shinjiezumi <shinjiezumi.work@gmail.com>
2021-06-19 15:08:56 +0900

    Fix

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -1,10 +1,10 @@
 <?php
-//
-//Breadcrumbs::register('top', function($breadcrumbs) {
-//    $breadcrumbs->push('Top', route('top'));
-//});
-//
-//Breadcrumbs::register('todo', function ($breadcrumbs) {
-//    $breadcrumbs->parent('top');
-//    $breadcrumbs->push('Todoリスト', route('todo'));
-//});
+
+Breadcrumbs::register('top', function($breadcrumbs) {
+    $breadcrumbs->push('Top', route('top'));
+});
+
+Breadcrumbs::register('todo', function ($breadcrumbs) {
+    $breadcrumbs->parent('top');
+    $breadcrumbs->push('Todoリスト', route('todo'));
+});
