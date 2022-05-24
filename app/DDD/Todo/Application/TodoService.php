a8152c84f54f5e5510275b7c4e89d8eeb51124ca
shinjiezumi <shinjiezumi.work@gmail.com>
2022-05-24 22:34:17 +0900

    fix

--- app/DDD/Todo/Application/TodoService.php
+++ app/DDD/Todo/Application/TodoService.php
@@ -10,11 +10,6 @@ use App\DDD\Todo\Domain\Model\ITodoRepository;
 class TodoService implements ITodoService
 {
     /**
-     * @var int 1ページ辺りのTodo表示数
-     */
-    private const TODO_PER_PAGE = 5;
-
-    /**
      * @var ITodoRepository
      */
     private $todoRepository;
@@ -34,6 +29,6 @@ class TodoService implements ITodoService
     public function get(TodoGetCommand $command)
     {
         $todos = $this->todoRepository->find();
-        return $todos->paginate(self::TODO_PER_PAGE);
+        return $todos->paginate($command->getPage());
     }
 }
