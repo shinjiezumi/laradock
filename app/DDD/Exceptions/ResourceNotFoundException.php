d23aa7f3da3d112b83a0ab4b704915e95c4e7eb7
shinjiezumi <shinjiezumi.work@gmail.com>
2022-06-02 21:17:44 +0900

    Add phpdoc

--- app/DDD/Exceptions/ResourceNotFoundException.php
+++ app/DDD/Exceptions/ResourceNotFoundException.php
@@ -4,8 +4,14 @@ namespace App\DDD\Exceptions;
 
 class ResourceNotFoundException extends \Exception
 {
+    /**
+     * 例外メッセージ
+     */
     const MESSAGE = 'リソースが見つかりませんでした';
 
+    /**
+     * @param string $message
+     */
     public function __construct(string $message = self::MESSAGE)
     {
         parent::__construct($message, 404, null);
