683ec87bea1f994f88e9a6ca7afcaffbc459b5b9
shinjiezumi <shinjiezumi.work@gmail.com>
2022-06-08 21:08:17 +0900

    Add phpdoc

--- app/DDD/Todo/Domain/Model/TodoId.php
+++ app/DDD/Todo/Domain/Model/TodoId.php
@@ -4,13 +4,22 @@ namespace App\DDD\Todo\Domain\Model;
 
 class TodoId
 {
+    /**
+     * @var int
+     */
     private $value;
 
+    /**
+     * @param int $id
+     */
     public function __construct(int $id)
     {
         $this->value = $id;
     }
 
+    /**
+     * @return int
+     */
     public function getId(): int
     {
         return $this->value;
